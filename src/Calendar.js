604e7b1b7a1069540d3221c3970ede4ee9e66dfe
youngcheolLee <zodiac0905@gmail.com>
2022-12-13 23:41:56 +0900

    Update Calendar

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 /**
  * Calendar.js 설명
  *  - 달력 폼 생성
@@ -7,35 +9,61 @@
 const date = new Date();
 const year = date.getFullYear();
 const month = date.getMonth() + 1;
-const day = date.getDay();
-console.log(day);
+const day = date.getDate();
+console.log(`today=${day}일`);
 
 const Calendar = () => {
+  // 날짜 데이터 관리용 state
+  const [calDate, setCalDate] = useState("");
+
+  // 이전 버튼 기능
+  const handleBoforeBtn = () => {};
+
+  // 이후 버튼 기능
+  const handleAfterBtn = () => {};
+
+  // 날짜 더미 데이터
+  const dummyDay = () => {
+    const myArray = [];
+    for (let i = 1; i < 32; i++) {
+      myArray.push(i);
+    }
+    return myArray;
+  };
+
   return (
     <div>
-      <h2>
-        {year}년 {month}월
-      </h2>
+      <div>
+        <button className="beforeBtn" onClick={handleBoforeBtn}>
+          ◀
+        </button>
+        <h2>
+          {year}년 {month}월
+        </h2>
+        <button className="afterBtn" onClick={handleAfterBtn}>
+          ▶
+        </button>
+      </div>
       <div>
         <table className="calTable">
-          <tr>
-            <th>Sunday</th>
-            <th>Monday</th>
-            <th>Tuesday</th>
-            <th>Wednesday</th>
-            <th>Thursday</th>
-            <th>Friday</th>
-            <th>Saturday</th>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-          </tr>
+          <thead>
+            <tr>
+              <th>Sunday</th>
+              <th>Monday</th>
+              <th>Tuesday</th>
+              <th>Wednesday</th>
+              <th>Thursday</th>
+              <th>Friday</th>
+              <th>Saturday</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dummyDay().map((element, idx) => (
+              <tr key={idx}>
+                <td>{element}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </div>
